29d440cb8e59118f326c48737fa5c983c6fa68d5
Thamara Liyanage <thamara.prabuddha@gmail.com>
2024-01-17 22:22:58 +0530

    update generate method

--- tasks/compositions.py
+++ tasks/compositions.py
@@ -1,9 +1,24 @@
+import copy
 from celery import shared_task
 
-from models import CompositionConfig
+from models import ComposedLayer, Composition, CompositionConfig
 from schemas import CompositionConfigSchema
 
 
 @shared_task
 def generate(data):
     comp_config: CompositionConfig = CompositionConfigSchema().load(data)  # type: ignore
+    comps: list[Composition] = []
+    visited: set[str] = set()
+    total: int = 0
+
+    for comp in comp_config.fixed_compositions:
+        comps.append(comp)
+        visited.add(comp.as_string())
+
+    while total < comp_config.total:
+        local_layers = copy.deepcopy(comp_config.layers)
+        composed_layers: list[ComposedLayer] = []
+
+        for layer in local_layers:
+            pass
